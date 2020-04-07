5f003df275a0dd1417bfe7a119dd1db8395a652f
nonbunrah <rj.bamrah@gmail.com>
2020-04-06 18:18:55 -0700

    did some button CSS

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
-let audience = document.querySelector('.audience-button');
-let host = document.querySelector('.host-button');
+let audience = document.querySelector('button');
 
 audience.addEventListener('click', function() {
-  let duration = 0.3, delay = 0.08;
+  let duration = 0.3, 
+      delay = 0.08;
   TweenMax.to(audience, duration, {scaleY: 1.6, ease: Expo.easeOut});
   TweenMax.to(audience, duration, {scaleX: 1.2, scaleY: 1, ease: Back.easeOut, easeParams:[3], delay: delay});
   TweenMax.to(audience, duration * 1.25, {scaleX: 1, scaleY: 1, ease: Back.easeOut, easeParams:[6], delay: delay * 3})
